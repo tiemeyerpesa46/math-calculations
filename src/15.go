2192f8fea6213802b7a63e89123659ef4cd987c9
tiemeyerpesa46 <tiemeyerpesa46@gmail.com>
2025-03-21 05:01:54 +0000

    Add 15.go

---
+++ src/15.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    // Add two numbers
+    sum := 5 + 3
+    fmt.Println("Sum:", sum)
+
+    // Subtract two numbers
+    difference := 7 - 2
+    fmt.Println("Difference:", difference)
+
+    // Multiply two numbers
+    product := 4 * 6
+    fmt.Println("Product:", product)
+
+    // Divide two numbers
+    quotient := 10 / 3
+    fmt.Println("Quotient:", quotient)
+}
