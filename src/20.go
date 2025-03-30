11489e31f8ef9790ff61db7cfebcd1f7aef7946d
tiemeyerpesa46 <tiemeyerpesa46@gmail.com>
2025-03-30 02:37:28 +0000

    Add 20.go

---
+++ src/20.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 5
+	b := 3
+	c := 7
+	d := 2
+
+	sum := a + b
+	subtraction := c - d
+
+	result := sum - subtraction
+	fmt.Println("The result is:", result)
+}
