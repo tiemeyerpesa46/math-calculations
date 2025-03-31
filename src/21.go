0979179964309f89cd4d9aee4680386d77a00fe5
tiemeyerpesa46 <tiemeyerpesa46@gmail.com>
2025-03-31 16:21:31 +0000

    Add 21.go

---
+++ src/21.go
@@ -0,0 +1,12 @@
+func main() {
+    sum := 2 + 3
+    difference := 7 - 2
+    product := 4 * 5
+    quotient := 10 / 2
+
+    // Print the results
+    fmt.Println("Sum: ", sum)
+    fmt.Println("Difference: ", difference)
+    fmt.Println("Product: ", product)
+    fmt.Println("Quotient:", quotient)
+}
