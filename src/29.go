7d9b0f9d1200b9b919a6366b77a9347d09e6462b
tiemeyerpesa46 <tiemeyerpesa46@gmail.com>
2025-04-09 04:41:41 +0000

    Add 29.go

---
+++ src/29.go
@@ -0,0 +1,13 @@
+func main() {
+    // Add two numbers
+    sum := 2 + 3
+
+    // Print the result
+    fmt.Println("The sum is:", sum)
+
+    // Subtract one number from another
+    diff := 4 - 2
+
+    // Print the result
+    fmt.Println("The difference is:", diff)
+}
