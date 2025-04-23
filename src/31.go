5e7fb1ddf694a96ffdf48ebdba4f89acd79a6f2e
tiemeyerpesa46 <tiemeyerpesa46@gmail.com>
2025-04-23 10:28:46 +0000

    Add 31.go

---
+++ src/31.go
@@ -0,0 +1,19 @@
+func main() {
+    // Addition of two numbers
+    sum := 5 + 3
+
+    // Subtraction of two numbers
+    difference := 8 - 2
+
+    // Multiplication of two numbers
+    product := 6 * 4
+
+    // Division of two numbers
+    quotient := 12 / 6
+
+    // Print the results
+    fmt.Println("Sum: ", sum)
+    fmt.Println("Difference: ", difference)
+    fmt.Println("Product: ", product)
+    fmt.Println("Quotient: ", quotient)
+}
